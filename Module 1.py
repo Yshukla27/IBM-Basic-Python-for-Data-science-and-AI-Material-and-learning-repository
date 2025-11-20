9783403895eb129cefee6ed8cae2237bb6422c8b
Yash shukla <newyashshukla15@gmail.com>
2025-11-20 16:07:28 +0530

    Update Module 1.py

--- Module 1.py
+++ Module 1.py
@@ -2,4 +2,21 @@ print('Hello World!!\n This Is My first Professional Course!!!')
 #What is the value of z where z = 2 + 15?
 z=2+15
 print(z)
+#Typecasting and datat-type check
+int("3.14")
+float("433542.24352")
+char(5.55454)
+type(343)
+type("Hello!!")
+type(float("234343"))
 
+# Store value into variable and uppend new info in the previosly created variable.
+
+x = 43 + 60 + 16 + 41
+print(x)
+x=x/60
+print(x)
+
+x=55-5
+x=x/10
+x
