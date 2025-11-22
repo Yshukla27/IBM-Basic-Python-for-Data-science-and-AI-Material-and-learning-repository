fd6187f2da26e5e10dc6efcbca12e7ac4fd972ff
Yash shukla <newyashshukla15@gmail.com>
2025-11-22 19:04:13 +0530

    Update Module 1.py

--- Module 1.py
+++ Module 1.py
@@ -20,3 +20,35 @@ print(x)
 x=55-5
 x=x/10
 x
+#Strings
+name='String is king'
+print(name[0])
+print(name[10])
+print(name[-1])
+print(name[-13])
+name[0:4]
+name[8:12]
+name[::2]
+name[0:5:2]
+statement = name + " is the best album"
+statement
+e = 'clocrkr1e1c1t'
+len(e)
+e[0:13:2]
+g = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb \
+Its fleece was white as snow And everywhere that Mary went Mary went, Mary went \
+Everywhere that Mary went The lamb was sure to go"
+g.find('snow')
+g.replace('Mary','Bob')
+import re
+s3 = "House number- 1105"
+# Write your code below and press Shift+Enter to execute
+# Use the search() function to search for the "\d" in the string
+result = re.search(r"\d", s3)
+
+# Check if a match was found
+if result:
+    print("Digit found")
+else:
+    print("Digit not found.")
+len("The BodyGuard")
