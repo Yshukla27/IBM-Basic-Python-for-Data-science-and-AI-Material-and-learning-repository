0d273848e6d35911161d3a9eca8e8c5f2ae30f5e
Yash shukla <newyashshukla15@gmail.com>
2026-02-18 01:03:24 +0530

    Create Module2.py

---
+++ Module2.py
@@ -0,0 +1,30 @@
+# Create a list
+
+L = ["The Bodyguard", 7.0, 1992]
+L
+
+a=[1,'hello',[1,2,3],True]
+a
+a[1]
+a[1:4]
+#List concatination
+A=[1,'a']
+B=[2,1,'d']
+A+B
+
+#Task 1 Shopping list. 
+Shopping_list=[]
+Shopping_list.append('Watch')
+Shopping_list.append('Laptop')
+Shopping_list.append('Shoes')
+Shopping_list.append('Pen')
+Shopping_list.append('Clothes')
+Shopping_list
+Shopping_list.append('Football')
+Shopping_list[0]
+Shopping_list[-1]
+Shopping_list[1:3]
+Shopping_list[3]='Notebook'
+Shopping_list
+del(Shopping_list[4])
+Shopping_list
