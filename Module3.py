c0e75612654edb21486cf3c43a5ec0755abc9a81
Yash shukla <newyashshukla15@gmail.com>
2026-02-18 19:17:36 +0530

    Create Module3.py

---
+++ Module3.py
@@ -0,0 +1,30 @@
+#Basoc Div function
+def division(a,b):
+    if(b==0):
+        print('Division by zero is undefined')
+        
+       
+    else:
+        return a/b
+    
+
+division(50,0)
+
+def con(a, b):
+    return(a + b)
+
+con('Hello','World!!1')
+
+con(10,20)
+
+d = ('a',1)
+e = ('b',2)
+con(d,e)
+
+#Count Function
+def count(a):
+    c=a.count('little')
+    return c
+
+x = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb.Its fleece was white as snow And everywhere that Mary went Mary went, Mary went Everywhere that Mary went The lamb was sure to go"
+count(x)
